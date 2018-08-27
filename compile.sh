adbfe80f868fa507f72a30ad8f2fa6dd4cb75a25
Daswf852 <daswf852@protonmail.com>
2018-08-27 16:49:03 +0300

    nani the fuck?

--- compile.sh
+++ compile.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-<<<<<<< HEAD
-g++ main.cpp VMManager.cpp R216.cpp PeripheralManager.cpp -o r2vm -g -O3 -fexpensive-optimizations -ldl -std=c++17
-=======
 g++ main.cpp VMManager.cpp R216.cpp PeripheralManager.cpp -o r2vm -O3 -fexpensive-optimizations -ldl
->>>>>>> e5a3600436c7c0d7219637cbca14820ce6fbb663
