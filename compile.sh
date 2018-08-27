3c39a5880dcc1c3b8da23bb25fd18b0e535a3c3d
Daswf852 <daswf852@protonmail.com>
2018-08-27 21:02:14 +0300

    lol

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-g++ main.cpp VMManager.cpp R216.cpp PeripheralManager.cpp -o r2vm -O3 -fexpensive-optimizations -ldl
+g++ main.cpp VMManager.cpp R216.cpp PeripheralManager.cpp -o r2vm -g -O3 -fexpensive-optimizations -ldl -std=c++17
